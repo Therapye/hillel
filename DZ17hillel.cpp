3aec62e077ca34baaa991f585bbec53440814b27
Oleksii <oleksii.zhyzhko@oa.edu.ua>
2025-12-16 23:38:30 +0200

    Add files via upload

---
+++ DZ17hillel.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Instrument {
+public:
+
+	Instrument() {};
+
+	virtual void playSound() {
+		cout << "Playing sound from Instrument" << endl;
+	};
+
+};
+
+class Piano : public Instrument {
+public:
+
+	Piano() {};
+
+	virtual void playSound() {
+		cout << "Playing sound from Piano" << endl;
+	};
+
+};
+
+class Guitar : public Instrument {
+public:
+	Guitar() {};
+
+	virtual void playSound() {
+		cout << "Playing sound from Guitar" << endl;
+	};
+
+};
+
+Guitar guitar;
+
+int main() {
+	
+	vector<Instrument*> orchestra;
+
+	orchestra.push_back(new Piano());
+	orchestra.push_back(new Guitar());
+
+	orchestra[0]->playSound();
+	orchestra[1]->playSound();
+
+    return 0;
+}
